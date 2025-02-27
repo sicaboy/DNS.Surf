adffc320e64052e2bd9476f535e7084d8c22a309
sicaboy <sica@qq.com>
2025-02-27 22:38:05 +1100

    cleanup

--- components/layouts/default.tsx
+++ components/layouts/default.tsx
@@ -1,5 +1,3 @@
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
 import { cn } from '@/lib/utils'
 
 export default function Layout({
@@ -11,13 +9,13 @@ export default function Layout({
 }>) {
   return (
     <main className="min-h-screen flex flex-col">
-      <Header />
+
       <section className="flex flex-1">
         <div className={cn('container mx-auto', className)}>
           {children}
         </div>
       </section>
-      <Footer />
+
     </main>
   )
 }
